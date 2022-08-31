d406c99564818266534e9ebb5aed1b7c4c5c0840
EmilieGanmont <77340577+EmilieGanmont@users.noreply.github.com>
2022-08-30 20:06:38 -0400

    Zork 1.2 NSWE Commands Commit
    Ensured the North, South, West, and East commands had respective written lines.

--- Zork/Program.cs
+++ Zork/Program.cs
@@ -10,12 +10,28 @@ namespace Zork
 
             string inputString = Console.ReadLine().Trim();
             Commands command = ToCommand(inputString);
-                
+
             if (command == Commands.Quit)
             {
                 Console.WriteLine("Thank you for playing.");
             }
-            else if(command == Commands.Look)
+            else if (command == Commands.North)
+            {
+                Console.WriteLine("You went North");
+            }
+            else if (command == Commands.South)
+            {
+                Console.WriteLine("You went South");
+            }
+            else if (command == Commands.East)
+            {
+                Console.WriteLine("You went East");
+            }
+            else if (command == Commands.West)
+            {
+                Console.WriteLine("You went West");
+            }
+            else if (command == Commands.Look)
             {
                 Console.WriteLine("This is an open field west of a white house, with a boarded front door. \nA rubber mat saying 'Welcome to Zork!' lies by the door.");
             }
@@ -27,15 +43,7 @@ namespace Zork
 
         static Commands ToCommand(string commandString)
         {
-            
-            if(Enum.TryParse<Commands>(commandString, true, out Commands command))
-            {
-                return command;
-            }
-            else
-            {
-                return Commands.Unknown;
-            }
+            return Enum.TryParse(commandString, true, out Commands result) ? result : Commands.Unknown;
           
         }
         
