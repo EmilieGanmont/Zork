7682c92a3cc62dbe6b37dfbaec693e930209dd26
EmilieGanmont <77340577+EmilieGanmont@users.noreply.github.com>
2022-09-21 15:18:22 -0400

    Zork 4.3 Initial Commit

--- Zork/Program.cs
+++ Zork/Program.cs
@@ -5,146 +5,136 @@ namespace Zork
 {
     class Program
     {
-        private static Room CurrentRoom
-        {
-            get
-            {
-                return _rooms[_location.Row, _location.Column];
-            }
-        }
+        // private static Room CurrentRoom
+        // {
+        //     get
+        //     {
+        //         return _rooms[_location.Row, _location.Column];
+        //     }
+        // }
 
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Zork!");
-            InitializeRoomDescription();
-
-            Room previousRoom = null;
-            Commands command = Commands.Unknown;
-            bool isRunning = true;
-            while (isRunning)
-            {
-                Console.WriteLine(CurrentRoom);
-                if(previousRoom != CurrentRoom && CurrentRoom.HasBeenVisited == false)
-                {
-                    Console.WriteLine(CurrentRoom.Description);
-                    previousRoom = CurrentRoom;
-                    CurrentRoom.HasBeenVisited = true;
-                }
-                Console.Write("> ");
-
-                command = ToCommand(Console.ReadLine().Trim());
-
-                string outputString;
-                switch (command)
-                {
-                    case Commands.Quit:
-                        isRunning = false;
-                        outputString = "Thank you for playing!";
-                        break;
-
-                    case Commands.Look:
-                        outputString = CurrentRoom.Description;
-                        break;
-
-                    case Commands.North:
-                    case Commands.South:
-                    case Commands.East:
-                    case Commands.West:
-                        if(Move(command))
-                        {
-                            outputString = $"You moved {command}.";
-                        }
-                        else
-                        {
-                            outputString = "The way is shut!";
-                        }
-                        break;
-
-                    default:
-                        outputString = "Unknown command.";
-                        break;
-                }
-
-                Console.WriteLine(outputString);
-            }
         }
+         //InitializeRoomDescription();
+         //
+         //Room previousRoom = null;
+         //Commands command = Commands.Unknown;
+         //bool isRunning = true;
+         //while (isRunning)
+         //{
+         //    Console.WriteLine(CurrentRoom);
+         //    if(previousRoom != CurrentRoom && CurrentRoom.HasBeenVisited == false)
+         //    {
+         //        Console.WriteLine(CurrentRoom.Description);
+         //        previousRoom = CurrentRoom;
+         //        CurrentRoom.HasBeenVisited = true;
+         //    }
+         //    Console.Write("> ");
+         //
+         //    command = ToCommand(Console.ReadLine().Trim());
+         //
+         //    string outputString;
+         //    switch (command)
+         //    {
+         //        case Commands.Quit:
+         //            isRunning = false;
+         //            outputString = "Thank you for playing!";
+         //            break;
+         //
+         //        case Commands.Look:
+         //            outputString = CurrentRoom.Description;
+         //            break;
+         //
+         //        case Commands.North:
+         //        case Commands.South:
+         //        case Commands.East:
+         //        case Commands.West:
+         //            if(Move(command))
+         //            {
+         //                outputString = $"You moved {command}.";
+         //            }
+         //            else
+         //            {
+         //                outputString = "The way is shut!";
+         //            }
+         //            break;
+         //
+         //        default:
+         //            outputString = "Unknown command.";
+         //            break;
+         //    }
+         //    Console.WriteLine(outputString);
+         //}
+        
 
-        static Commands ToCommand(string commandString)
-        {
-            return Enum.TryParse(commandString, true, out Commands result) ? result : Commands.Unknown;
-          
-        }
+      //static Commands ToCommand(string commandString)
+      //{
+      //    return Enum.TryParse(commandString, true, out Commands result) ? result : Commands.Unknown;
+      //  
+      //}
 
-        private static bool Move(Commands command)
-        {
-            Assert.IsTrue(IsDirection(command), "Invalid direction");
-
-            bool didMove = false;
-
-            switch (command)
-            {
-                case Commands.North when _location.Row < _rooms.GetLength(0) - 1: 
-                    _location.Row++;
-                    didMove = true;
-                    break;
-
-                case Commands.South when _location.Row > 0:
-                    _location.Row--;
-                    didMove = true;
-                    break;
-
-                case Commands.East when _location.Column < _rooms.GetLength(1) - 1: 
-                    _location.Column++;
-                    didMove = true;       
-                    break;
-
-                case Commands.West when _location.Column > 0:
-                    _location.Column--;
-                    didMove = true;
-                    break;
-            }
-
-            return didMove;
-        }
-
-        private static void InitializeRoomDescription()
-        {
-            var roomMap = new Dictionary<string, Room>();
-            foreach (Room room in _rooms)
-            {
-                roomMap[room.Name] = room;
-            }
-
-            roomMap["Rocky Trail"].Description = "You are on a rock strewn trail.";
-            roomMap["South of House"].Description = "You are facing the south side of a white house. There is no door here, and all the windows are barred.";
-            roomMap["Canyon View"].Description = "You are at the top of the Great Canyon on its south wall.";
-            
-            roomMap["Forest"].Description = "This is a forest, with trees in all directions around you.";
-            roomMap["West of House"].Description = "This is an open field west of a white house, with a boarded front door.";
-            roomMap["Behind House"].Description = "You are behind the white house. In one corner of the house is a small window which is slightly ajar.";
-            
-            roomMap["Dense Woods"].Description = "This is a dimly lit forest, with large trees all around. To the east, there appears to be sunlight.";
-            roomMap["North of House"].Description = "You are facing the north side of a white house. There is no door here, and all the windows are barred.";
-            roomMap["Clearing"].Description = "You are in a clearing, with a forest surrounding you on the west and south.";
-        }
-        private static bool IsDirection(Commands command) => Directions.Contains(command);
-
-        private static readonly Room[,] _rooms = {
-            {new Room("Rocky Trail"), new Room("South of House"), new Room("Canyon View") },
-            {new Room("Forest"), new Room("West of House"), new Room("Behind House") },
-            {new Room("Dense Woods"), new Room("North of House"), new Room("Clearing") }
-        };
-
-
-        private static readonly List<Commands> Directions = new List<Commands>
-        {
-            Commands.North,
-            Commands.South,
-            Commands.East,
-            Commands.West
-        };
-
-        private static (int Row, int Column) _location = (1, 1);
+      //  private static bool Move(Commands command)
+      //  {
+      //      Assert.IsTrue(IsDirection(command), "Invalid direction");
+      //
+      //      bool didMove = false;
+      //
+      //      switch (command)
+      //   {
+      //       case Commands.North when _location.Row < _rooms.GetLength(0) - 1: 
+      //           _location.Row++;
+      //           didMove = true;
+      //           break;
+      //
+      //       case Commands.South when _location.Row > 0:
+      //           _location.Row--;
+      //           didMove = true;
+      //           break;
+      //
+      //       case Commands.East when _location.Column < _rooms.GetLength(1) - 1: 
+      //           _location.Column++;
+      //           didMove = true;       
+      //           break;
+      //
+      //       case Commands.West when _location.Column > 0:
+      //           _location.Column--;
+      //           didMove = true;
+      //           break;
+      //   }
+      //
+      //      return didMove;
+      //  }
+      //  private static void InitializeRoomDescription()
+      //  {
+      //      var roomMap = new Dictionary<string, Room>();
+      //      foreach (Room room in _rooms)
+      //      {
+      //          roomMap[room.Name] = room;
+      //      }
+      //      roomMap["Rocky Trail"].Description = "You are on a rock strewn trail.";
+      //      roomMap["South of House"].Description = "You are facing the south side of a white house. There is no door here, and all the windows are barred.";
+      //      roomMap["Canyon View"].Description = "You are at the top of the Great Canyon on its south wall.";
+      //      
+      //      roomMap["Forest"].Description = "This is a forest, with trees in all directions around you.";
+      //      roomMap["West of House"].Description = "This is an open field west of a white house, with a boarded front door.";
+      //      roomMap["Behind House"].Description = "You are behind the white house. In one corner of the house is a small window which is slightly ajar.";
+      //      
+      //      roomMap["Dense Woods"].Description = "This is a dimly lit forest, with large trees all around. To the east, there appears to be sunlight.";
+      //      roomMap["North of House"].Description = "You are facing the north side of a white house. There is no door here, and all the windows are barred.";
+      //      roomMap["Clearing"].Description = "You are in a clearing, with a forest surrounding you on the west and south.";
+      //  }
+      //  private static bool IsDirection(Commands command) => Directions.Contains(command);
+      //  private static readonly List<Commands> Directions = new List<Commands>
+      //  {
+      //      Commands.North,
+      //      Commands.South,
+      //      Commands.East,
+      //      Commands.West
+      //  };
+      //
+      //  private static (int Row, int Column) _location = (1, 1);
     }
 
 }
