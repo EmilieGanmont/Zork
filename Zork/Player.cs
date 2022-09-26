772de46daca4f919beca18ba23fa2c696371cbfe
EmilieGanmont <77340577+EmilieGanmont@users.noreply.github.com>
2022-09-26 14:42:14 -0400

    Revert "Zork 4.1 Second Commit"
    This reverts commit 719f5f8b9b6a80c14c54c9eb5db5fc028d2ae316.

--- Zork/Player.cs
+++ Zork/Player.cs
@@ -23,39 +23,6 @@ namespace Zork
         }
 
         private World _world;
-
-        private static bool Move(Commands command)
-        {
-            Assert.IsTrue(IsDirection(command), "Invalid direction");
-
-            bool didMove = false;
-
-            switch (command)
-            {
-                case Commands.North when _location.Row < _world.Rooms.GetLength(0) - 1:
-                    _location.Row++;
-                    didMove = true;
-                    break;
-
-                case Commands.South when _location.Row > 0:
-                    _location.Row--;
-                    didMove = true;
-                    break;
-
-                case Commands.East when _location.Column < _world.Rooms.GetLength(1) - 1:
-                    _location.Column++;
-                    didMove = true;
-                    break;
-
-                case Commands.West when _location.Column > 0:
-                    _location.Column--;
-                    didMove = true;
-                    break;
-            }
-
-            return didMove;
-        }
-
         private static (int Row, int Column) _location = (1, 1);
 
     }
