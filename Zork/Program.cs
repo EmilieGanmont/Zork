a994b9d4357acc59265b39b0aa693ce3adaa6854
EmilieGanmont <77340577+EmilieGanmont@users.noreply.github.com>
2022-09-27 18:47:50 -0400

    Zork 4.2 json added

--- Zork/Program.cs
+++ Zork/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using Newtonsoft.Json;
 namespace Zork
 {
     class Program
@@ -17,7 +18,7 @@ namespace Zork
         {
             Console.WriteLine("Welcome to Zork!");
 
-            const string defaultRoomsFilename = @"Content\Rooms.txt";
+            const string defaultRoomsFilename = @"Content\Rooms.json";
             string roomsFilename = (args.Length > 0 ? args[(int)CommandLineArguments.RoomsFilename] : defaultRoomsFilename);
             InitializeRoomDescription(roomsFilename);
 
@@ -109,38 +110,14 @@ namespace Zork
 
             return didMove;
         }
-        private static readonly Dictionary<string, Room> RoomMap;
 
-        static Program()
-        {
-            RoomMap = new Dictionary<string, Room>();
-            foreach (Room room in _rooms)
-            {
-                RoomMap[room.Name] = room;
-            }
-        }
         private static void InitializeRoomDescription(string roomsFilename)
         {
-            const string fieldDelimiter = "##";
-            const int expectedFieldCount = 2;
-
-            string[] lines = File.ReadAllLines(roomsFilename);
-            foreach (string line in lines)
-            {
-                string[] fields = line.Split(fieldDelimiter);
-                if (fields.Length != expectedFieldCount)
-                {
-                    throw new InvalidDataException("Invalid record.");
-                }
-                string name = fields[(int)Fields.Name];
-                string description = fields[(int)Fields.Description];
-
-                RoomMap[name].Description = description;
-            }
+                _rooms = JsonConvert.DeserializeObject<Room[,]>(File.ReadAllText(roomsFilename));
         }
         private static bool IsDirection(Commands command) => Directions.Contains(command);
 
-        private static readonly Room[,] _rooms = {
+        private static Room[,] _rooms = {
             {new Room("Rocky Trail"), new Room("South of House"), new Room("Canyon View") },
             {new Room("Forest"), new Room("West of House"), new Room("Behind House") },
             {new Room("Dense Woods"), new Room("North of House"), new Room("Clearing") }
