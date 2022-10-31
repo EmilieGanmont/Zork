80a518b91baaf260c5383d8700cf8b9027c2c33f
EmilieGanmont <77340577+EmilieGanmont@users.noreply.github.com>
2022-10-31 14:25:31 -0400

    Zork 9 Take and Drop Commands

--- Zork/Game.cs
+++ Zork/Game.cs
@@ -32,7 +32,6 @@ namespace Zork
                     Console.WriteLine(Player.Location.Description);
                     previousRoom = Player.Location;
                     ShowItems();
-
                 }
 
                 Console.Write("\n> ");
@@ -44,16 +43,26 @@ namespace Zork
                 string verb = null;
                 string subject = null;
 
-                if(commandTokens.Length == 0)
-                {
-                    continue;
-                }
-                else if(commandTokens.Length == 1)
-                {
-                    //Insert code here
-                }
+                Commands command = Commands.Unknown;
 
-                Commands command = ToCommand(inputString);
+                switch (commandTokens.Length)
+                {
+                    case 0:
+                        continue;
+
+                    case 1:
+                        verb = commandTokens[0];
+                        command = ToCommand(verb);
+                        break;
+
+                    case 2:
+                        verb = commandTokens[0];
+                        subject = commandTokens[1];
+
+                        command = ToCommand(verb);
+                        break;
+
+                }
 
                 switch (command)
                 {
@@ -71,20 +80,20 @@ namespace Zork
                     case Commands.East:
                     case Commands.West:
                         Directions direction = Enum.Parse<Directions>(command.ToString(), true);
-                        if (Player.Move(direction) == false )
+                        if (Player.Move(direction) == false)
                         {
                             Console.WriteLine("The way is shut!");
                         }
                         break;
                     case Commands.Inventory:
-                        if(Player.Inventory.Count <= 0 )
+                        if (Player.Inventory.Count <= 0)
                         {
                             Console.WriteLine("You are empty handed.");
                             Console.Write("\n> ");
                         }
-                        else if(Player.Inventory.Count > 0)
+                        else if (Player.Inventory.Count > 0)
                         {
-                            foreach(Item i in Player.Inventory)
+                            foreach (Item i in Player.Inventory)
                             {
                                 Console.WriteLine(i.Description);
                                 Console.Write("\n> ");
@@ -92,11 +101,66 @@ namespace Zork
                         }
                         break;
                     case Commands.Take:
+                        if (subject != null)
+                        {
+                            foreach (Item i in Player.Location.Inventory)
+                            {
+                                if (subject.Equals(i.Name))
+                                {
+                                    Player.Inventory.Add(i);
+                                    Player.Location.Inventory.Remove(i);
+
+                                    Console.WriteLine("Taken.");
+                                    break;
+                                }
+                                else if(subject.Equals(i.Name) != true)
+                                {
+                                    Console.WriteLine("There is no such thing");
+                                    break;
+                                }
+                            }
+                        }
+                        else if (subject == null)
+                        {
+                            Console.WriteLine("Take what?");
+                        }
+                        break;
+
+                    case Commands.Drop:
+                        if (subject != null)
+                        {
+                            foreach (Item i in Player.Inventory)
+                            {
+                                if (subject.Equals(i.Name))
+                                {
+                                    Player.Inventory.Remove(i);
+                                    Player.Location.Inventory.Add(i);
+
+                                    Console.WriteLine("Dropped.");
+                                    break;
+                                }
+                                else if(subject.Equals(i.Name) == false)
+                                {
+                                    Console.WriteLine("You don't have that thing");
+                                    break;
+                                }
+                            }
+                        }
+                        else if (subject == null)
+                        {
+                            Console.WriteLine("Drop what?");
+                        }
+
+                            if(Player.Inventory.Count == 0 )
+                            {
+                                Console.WriteLine("You don't have that thing.");
+                            }
                         break;
 
                     default:
                         Console.WriteLine("Unknown command");
                         break;
+
                 }
             }
         }
--- Zork/Program.cs
+++ Zork/Program.cs
@@ -1,7 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
+﻿using System.IO;
 using Newtonsoft.Json;
+using System;
+
 namespace Zork
 {
     class Program
