ae06fac60b0841896c07d32324dad842d75db256
EmilieGanmont <77340577+EmilieGanmont@users.noreply.github.com>
2022-11-15 20:32:42 -0500

    Zork Unity Integration

---
+++ Zork.Unity/Assets/Scripts/GameManager.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using Newtonsoft.Json;
+using Zork.Common;
+using TMPro;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField]
+    private TextMeshProUGUI Location, Score, Moves;
+
+    [SerializeField]
+    private UnityInputService Input;
+
+    [SerializeField]
+    private UnityOutputService Output; 
+    
+    private void Awake()
+    {
+        TextAsset gameJson = Resources.Load<TextAsset>("Game Json");
+        _game = JsonConvert.DeserializeObject<Game>(gameJson.text);
+        _game.Run(Input, Output);
+    }
+
+    private Game _game;
+}
